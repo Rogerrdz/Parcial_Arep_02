b2ce97b5da3f2085da3a40f22bf09af89f552127
unknown <roger.rodriguez-a@labinfo.is.escuelaing.edu.co>
2026-03-24 15:45:48 -0500

    readme

--- src/main/java/org/example/service/HttpService.java
+++ src/main/java/org/example/service/HttpService.java
@@ -9,39 +9,27 @@ import java.net.URL;
 
 public class HttpService {
     private static final String USER_AGENT = "Mozilla/5.0";
-
         public String get(String urlStr) {
-
         try {
             URL obj = new URL(urlStr);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
             con.setRequestMethod("GET");
-
             con.setRequestProperty("User-Agent", USER_AGENT);
-
             int responseCode = con.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
-
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(con.getInputStream()));
-
                 String inputLine;
                 StringBuffer response = new StringBuffer();
-
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
-
                 in.close();
-
                 return response.toString();
-
             } else {
                 throw new RuntimeException("GET request failed: " + responseCode);
             }
-
         } catch (Exception e) {
             throw new RuntimeException("Error calling service: " + urlStr, e);
         }
