41f7fb7d5801384be85791933484106f45b76b78
Roger rdz <142515714+Rogerrdz@users.noreply.github.com>
2026-03-24 14:17:05 -0500

    Arreglando pom con SpringBoot y colocando el modelo matematico

--- src/main/java/org/example/model/Math.java
+++ src/main/java/org/example/model/Math.java
@@ -1,5 +1,25 @@
 package org.example.model;
 
 public class Math {
-    
+    private String operation;
+    private int input;
+    private String output;
+
+    public Math(String operation, int input, String output) {
+        this.operation = operation;
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
 }
